37aed92307a7d7f5b6ecd2ebed2135be4fce70fc
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2023-06-02 22:22:47 -0400

    rebuild

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,32 +5,16 @@ const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        {/* <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
-          </p>
-        </div> */}
-
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            Christian<br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Noble Shriver</span>{" "}
+            Hello,<br className="sm:block hidden" />{" "}
+            <span className="text-gradient">I'm Chris.</span>{" "}
           </h1>
-          {/* <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
-          </div> */}
         </div>
-
-        {/* <h1 className="font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Shriver
-        </h1> */}
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          10x programmer extraordinaire.
+          Junior CS student at UMass Amherst.
         </p>
       </div>
-
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robotwithball} alt="robot" className="w-[100%] h-[100%] top-12 relative z-[5]" />
 
@@ -40,10 +24,6 @@ const Hero = () => {
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
       </div>
-
-      {/* <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted />
-      </div> */}
     </section>
   );
 };
